db250e7e72b3c5788adb9c0a7561515acb64da03
Cal Barkman <callahan.barkman@gmail.com>
2025-10-04 11:39:39 -0700

    Problem 003

---
+++ BitWise-Operations/problem_003.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int isKthBitSet(int n, int k) {
+    // Write your code here
+    if(n & (1<<k)){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    printf("%d", isKthBitSet(n, k));
+    return 0;
+}
